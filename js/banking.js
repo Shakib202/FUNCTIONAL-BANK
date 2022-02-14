fc0745895a4ef70afd82387c6d522125b7175271
Shakib202 <shakib2020360@gmail.com>
2022-02-15 04:26:13 +0600

    fifth-commit

--- js/banking.js
+++ js/banking.js
@@ -6,6 +6,7 @@ const fiveDouble = doubleIt(5);
 const severDouble = doubleIt(7);
 
 function getInputValue(inputId){
+  // debugger;
   const inputField = document.getElementById(inputId);
   const inputAmountText = inputField.value;
   const amountValue = parseFloat(inputAmountText);
@@ -22,10 +23,19 @@ function updateTotalField(totalFieldId, amount){
   totalElement.innerText= previousTotal + amount;
 }
 
-function updateBalance(amount, isAdd){
+function getCurrentBalance(){
   const balanceTotal = document.getElementById('balance-total');
   const balanceTotalText = balanceTotal.innerText;
   const previousBalanceTotal = parseFloat(balanceTotalText);
+  return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd){
+  const balanceTotal = document.getElementById('balance-total');
+  /*  
+  const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText); */
+const previousBalanceTotal = getCurrentBalance();
 if(isAdd == true){
   balanceTotal.innerText = previousBalanceTotal + amount;
 }
@@ -36,18 +46,21 @@ else {
 
 document.getElementById('deposit-button').addEventListener('click', function(){
   const depositAmount = getInputValue('deposit-input');
-  // get and update deposit total
-  updateTotalField('deposit-total', depositAmount);
-  // update balance
-  updateBalance(depositAmount, true);
+  if (depositAmount > 0){
+    updateTotalField('deposit-total', depositAmount);
+    updateBalance(depositAmount, true);
+  }
 });
 
 // handle withdraw button
 document.getElementById('withdraw-button').addEventListener('click', function(){
   const withdrawAmount = getInputValue('withdraw-input');
-  // get and update withdraw total
-  updateTotalField('withdraw-total' ,withdrawAmount);
-
-  // update balance after withdraw
-  updateBalance(withdrawAmount, false);
+  const currentBalance = getCurrentBalance();
+  if(withdrawAmount > 0 && withdrawAmount < currentBalance){
+    updateTotalField('withdraw-total' ,withdrawAmount);
+    updateBalance(withdrawAmount, false);
+  }
+  if(withdrawAmount > currentBalance){
+    console.log('You can not withdraw more than what you have in your account');
+  }
 })
